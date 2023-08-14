4c7bbe5ab2325d1f295175ed72fce20dfcc00c92
junghan0611 <junghanacs@gmail.com>
2023-08-14 22:19:35 +0900

    revert evil

--- layers/+distributions/spacemacs-bootstrap/packages.el
+++ layers/+distributions/spacemacs-bootstrap/packages.el
@@ -29,7 +29,7 @@
     (bind-map :step bootstrap)
     (bind-key :step bootstrap)
     (diminish :step bootstrap)
-    (evil :location (recipe :fetcher local) :step bootstrap)
+    (evil :step bootstrap)
     (hydra :step bootstrap)
     (use-package :step bootstrap)
     (which-key :step bootstrap)
