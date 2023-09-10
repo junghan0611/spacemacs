f8c353d87375aada127529a735995c9995968c3c
junghan0611 <junghanacs@gmail.com>
2023-09-10 10:11:47 +0900

    revert org

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -55,10 +55,11 @@
     org-present
     org-cliplink
     org-rich-yank
-    (org-projectile 
-      :location (recipe :fetcher github :repo "junghan0611/org-project-capture"
-                        :branch "main")
-      :requires projectile)
+    (org-projectile :requires projectile)
+    ;; (org-projectile
+    ;;   :location (recipe :fetcher github :repo "junghan0611/org-project-capture"
+    ;;                     :branch "main")
+    ;;   :requires projectile)
     (ox-epub :toggle org-enable-epub-support)
     (ox-twbs :toggle org-enable-bootstrap-support)
     ;; use a for of ox-gfm to fix index generation
